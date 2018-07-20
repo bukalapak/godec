24f6b1d4ffebb0a4d7b20e759804278539798648
Indra Saputra <flame.fire99@gmail.com>
2018-07-20 13:51:09 +0700

    doc for execute

--- executor.go
+++ executor.go
@@ -1,4 +1,16 @@
 package godec
 
+import "context"
+
 type executor struct {
+}
+
+// Execute executes given godec interface to create a new golang interface using given template.
+// The new file will be located in folder decorator, relative to current directory.
+// The new file's name will be `{template.Name}/{intf.Name}.go`.
+//
+// Please, keep in mind that the template will be loaded from `$GOPATH/src/github.com/bukalapak/godec/template` folder.
+// It will look for `{template.Name}.go.tmpl` in that folder.
+func (e *executor) Execute(ctx context.Context, intf *Interface, template *Template) error {
+
 }
