7d7588e91da4972e4be1732ba8481d5da0727386
Indra Saputra <flame.fire99@gmail.com>
2018-07-20 14:02:40 +0700

    create interface tempalte and format using gofmt

--- executor.go
+++ executor.go
@@ -3,6 +3,7 @@ package godec
 import (
 	"context"
 	"os"
+	"os/exec"
 	"path"
 	"strings"
 	"text/template"
@@ -18,12 +19,14 @@ const (
 type executor struct {
 }
 
-// Execute executes given godec interface to create a new golang interface using given template.
-// The new file will be located in folder decorator, relative to current directory.
-// The new file's name will be `{tmpl.Name}/{intf.Name}.go`.
+// Execute executes given godec interface to generate a new golang interface using given template.
+// The generated file will be located in folder decorator, relative to current directory.
+// The generated file's name will be `{tmpl.Name}/{intf.Name}.go`.
 //
 // Please, keep in mind that the template will be loaded from `$GOPATH/src/github.com/bukalapak/godec/template` folder.
 // It will look for `{tmpl.Name}.go.tmpl` in that folder.
+//
+// For convenience, the generated file will automatically be formatted using `gofmt -s w`.
 func (e *executor) Execute(ctx context.Context, intf *Interface, tmpl *Template) error {
 	t, err := template.ParseFiles(path.Join(os.Getenv("GOPATH"), templatePath, tmpl.Name+".go.tmpl"))
 	if err != nil {
@@ -40,4 +43,10 @@ func (e *executor) Execute(ctx context.Context, intf *Interface, tmpl *Template)
 	if err != nil {
 		return errors.Wrap(err, "couldn't create decorator file")
 	}
+
+	if err = t.Execute(file, intf); err != nil {
+		return errors.Wrap(err, "couldn't make decorator from template")
+	}
+
+	return exec.CommandContext(ctx, "gofmt", "-s", "-w", fileName).Run()
 }
