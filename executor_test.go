c94ac72d2ca54b4867c80b8501fa97a483459e74
Indra Saputra <flame.fire99@gmail.com>
2018-07-23 16:02:21 +0700

    test for Execute

--- executor_test.go
+++ executor_test.go
@@ -1,6 +1,7 @@
 package godec_test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,4 +12,29 @@ import (
 func TestNewExecutor(t *testing.T) {
 	executor := godec.NewExecutor()
 	assert.Implements(t, (*godec.Executor)(nil), executor)
+}
+
+func Test_executor_Execute(t *testing.T) {
+	intf := &godec.Interface{
+		Name:        "AnInterface",
+		Package:     "something",
+		PackagePath: "github.com/you/something",
+	}
+
+	template := &godec.Template{
+		Name: "canceler",
+	}
+
+	executor := godec.NewExecutor()
+
+	// template found
+	err := executor.Execute(context.Background(), intf, template)
+	assert.Nil(t, err)
+
+	// template not found
+	notFoundTemplate := &godec.Template{
+		Name: "404Template",
+	}
+	err = executor.Execute(context.Background(), intf, notFoundTemplate)
+	assert.NotNil(t, err)
 }
