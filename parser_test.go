f888e32f2334fad36b2b1352b1ff8260852a9fc3
Indra Saputra <flame.fire99@gmail.com>
2018-07-23 16:45:09 +0700

    test success parser

--- parser_test.go
+++ parser_test.go
@@ -1,6 +1,9 @@
 package godec_test
 
 import (
+	"context"
+	"os"
+	"path"
 	"testing"
 
 	"github.com/bukalapak/godec"
@@ -13,4 +16,26 @@ func TestNewParser(t *testing.T) {
 }
 
 func Test_parser_Parse(t *testing.T) {
+	parser := godec.NewParser()
+
+	file := &godec.File{
+		Location:  path.Join(os.Getenv("GOPATH"), "src/github.com/bukalapak/godec/godec.go"),
+		Interface: "Parser",
+	}
+
+	intf, err := parser.Parse(context.Background(), file)
+	assert.Nil(t, err)
+
+	assert.Equal(t, "Parser", intf.Name)
+	assert.Equal(t, "godec", intf.Package)
+	assert.Equal(t, "github.com/bukalapak/godec", intf.PackagePath)
+	assert.Equal(t, "Parse", intf.Methods[0].Name)
+	assert.Equal(t, "a", intf.Methods[0].Params[0].Name)
+	assert.Equal(t, "context.Context", intf.Methods[0].Params[0].Type)
+	assert.Equal(t, "b", intf.Methods[0].Params[1].Name)
+	assert.Equal(t, "*godec.File", intf.Methods[0].Params[1].Type)
+	assert.Equal(t, "*godec.Interface", intf.Methods[0].ReturnValues[0].Type)
+	assert.Equal(t, "nil", intf.Methods[0].ReturnValues[0].ZeroValue)
+	assert.Equal(t, "error", intf.Methods[0].ReturnValues[1].Type)
+	assert.Equal(t, "nil", intf.Methods[0].ReturnValues[1].ZeroValue)
 }
