de7dfca3a3af98aea52290177a671aa9410ea5a3
Indra Saputra <flame.fire99@gmail.com>
2018-07-23 17:36:42 +0700

    test not found template

--- decorator_test.go
+++ decorator_test.go
@@ -34,3 +34,35 @@ func Test_decorator_Decorate_Success(t *testing.T) {
 	err := decorator.Decorate(context.Background(), file, template)
 	assert.Nil(t, err)
 }
+
+func Test_decorator_Decorate_Error(t *testing.T) {
+	// not found file
+	notFoundfile := &godec.File{
+		Location:  path.Join(os.Getenv("GOPATH"), "src/github.com/bukalapak/godec/testdata/notfound.go"),
+		Interface: "Sample",
+	}
+
+	template := &godec.Template{
+		Name: "canceler",
+	}
+
+	parser := godec.NewParser()
+	executor := godec.NewExecutor()
+	decorator := godec.NewDecorator(parser, executor)
+
+	err := decorator.Decorate(context.Background(), notFoundfile, template)
+	assert.NotNil(t, err)
+
+	// not found template
+	file := &godec.File{
+		Location:  path.Join(os.Getenv("GOPATH"), "src/github.com/bukalapak/godec/testdata/sample.go"),
+		Interface: "Sample",
+	}
+
+	notFoundTemplate := &godec.Template{
+		Name: "notfoundtemplate",
+	}
+
+	err = decorator.Decorate(context.Background(), file, notFoundTemplate)
+	assert.NotNil(t, err)
+}
