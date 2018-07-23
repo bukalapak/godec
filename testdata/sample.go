56c98a4fcfd76464c012a5e3360a7f2d014278e8
Indra Saputra <flame.fire99@gmail.com>
2018-07-23 17:13:25 +0700

    add Something struct to test getType

--- testdata/sample.go
+++ testdata/sample.go
@@ -3,9 +3,13 @@ package testdata
 type Struct struct {
 }
 
+type Something struct {
+}
+
 type Sample interface {
 	A(s Struct) (int, error)
 	B(s Struct) (Struct, error)
 	C(s Struct) float64
 	D(s Struct) string
+	E(s Struct, st *Something) *Something
 }
